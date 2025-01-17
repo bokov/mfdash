6ae2ae4361e7d191b6cbec2b8886061ac27b5c33
Alex Bokov <bokov@uthscsa.edu>
2025-01-17 16:12:28 -0600

    server.R; features; Relies on global.R to create market_table (other DTs still created here). Creates bets_table for questions selected to expand into time-series. More efficient handling of already-downloaded data.

--- server.R
+++ server.R
@@ -6,20 +6,7 @@ shinyServer(function(input, output, session) {
   rv <- reactiveValues(details = NULL)
   
   # Prepare datatable with buttons
-  output$market_table <- renderDT({
-    market_data %>% 
-      transmute(
-        closeTime #, question, url
-        , outcomeType, uniqueBettorCount
-        , lastUpdatedTime, probability
-        , question = sprintf('<a href="%s" target="_blank">%s</a>',url,question)
-        ,action = sprintf('<button class="btn btn-success btn-sm action-button" id="btn_%s">
-                          <span>&#9654;</span></button>',slug)) %>% 
-#      select(closeTime, question, url, outcomeType, uniqueBettorCount, lastUpdatedTime, probability, action) %>% 
-      datatable(escape = FALSE
-                , options = list(scrollY = "400px", paging = FALSE)
-                , rownames='',selection='none')
-  }, server = TRUE)
+  output$market_table <- renderDataTable(market_data_for_DT, server = TRUE);
   
   # Prepare details datatable
   output$details_table <- renderDT({
@@ -38,18 +25,42 @@ shinyServer(function(input, output, session) {
           '<span>&#128200;</span></button>'
         )
       ) %>% 
-      datatable(escape = FALSE, options = list(scrollY = "400px", paging = FALSE)
+      datatable(escape = FALSE
+                , options = list(scrollY = "400px", scrollX= T, scrollCollapse=T
+                                 , fixedHeader=T, paging = FALSE)
                 ,rownames = '', selection='none')
-  })
+  });
+  
+  output$bets_table <- renderDT({
+    if(is.null(rv$prices)||nrow(rv$prices)==0){
+      return(datatable(data.frame(Message='Select some markets or answers'
+                                  ,options=list(dom='t'),rownames = '')))
+    } else {
+      rv$prices %>% transmute(contractId,question,answerId,text) %>% unique() %>%
+        datatable(options=list(scrollY='400px',paging=F,rownames=''));
+    };
+  });
   
   # Handle custom input 'slugToAdd'
   observeEvent(input$slugToAdd, {
-    slug <- input$slugToAdd
-    slugDetails <- try(expandDetails(slug), silent = TRUE)
-    if (!inherits(slugDetails, 'try-error')) {
-      rv$details <- bind_rows(rv$details, slugDetails) %>% distinct()
-    }
-  })
+    slug <- input$slugToAdd;
+    if(is.null(rv$details) || !slug %in% rv$details$slug){
+      # TODO simply check for slug already existing in rv$details and skip if
+      #      it does or if rv$details is null
+      slugDetails <- try(expandDetails(slug), silent = TRUE);
+      if (!inherits(slugDetails, 'try-error')) {
+        # insert dummy values for numeric markets
+        if(!hasName(slugDetails,'id.answer')) slugDetails$id.answer <- '';
+        if(!hasName(slugDetails,'text')) slugDetails$text <- '';
+        # update the details reactive value 
+        rv$details <- bind_rows(rv$details, slugDetails) %>% distinct();
+        message("Downloaded details for:",slug);
+      } else {
+        message("slug ... ",slugDetails);
+        browser();
+        };
+    } else message("Details already cached for:",slug);
+  });
   
   # Handle removal of details
   observeEvent(input$detailToRemove, {
@@ -60,10 +71,27 @@ shinyServer(function(input, output, session) {
   
   # Handle plotting of details
   observeEvent(input$detailToPlot, {
-    detailID <- input$detailToPlot;
-    message(paste("Plotting detail ID:", detailID))
+    pricesID <- input$detailToPlot;
+    if(is.null(rv$prices)||!pricesID %in% rv$prices$contractId){
+      newPrices <- try(manifold_api(endpoint='/v0/bets',request_type='GET'
+                                ,params_list=list(contractId=pricesID
+                                                  ,includeZeroShareRedemptions='true'
+                                                  ,filterRedemptions='false'))$content %>% 
+        lapply(flattenmarketlistdata) %>% bind_rows());
+      if(!inherits(newPrices,'try-error')){
+        # insert dummy values for numeric markets
+        if(!hasName(newPrices,'answerId')) newPrices$answerId <- '';
+        newPrices <- left_join(newPrices
+                               ,rv$details[,c('id.answer','id','question','text')]
+                               ,by=c(answerId='id.answer',contractId='id')
+                               ,suffix=c('.bet','.details'));
+        rv$prices <- bind_rows(rv$prices, newPrices) %>% distinct();
+        message("Downloaded prices for ID:", pricesID);
+      } else message(newPrices);
+    } else message("Prices already cached for ID:", pricesID);
     # Add your plotting logic here
   })
   
   observeEvent(input$debug,browser());
+  
 })
